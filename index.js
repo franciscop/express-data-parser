6a39212ed24ab7de1c7fc1a9fdff56c4ecd36219
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2016-10-05 18:45:22 +0900

    Init code

---
+++ index.js
@@ -0,0 +1,22 @@
+var formidable = require('formidable');
+
+module.exports = function(options = {}){
+
+  return function(req, res, next){
+    if (req.method === 'GET') return next();
+    if (!requestType.includes('multipart/form-data')) next();
+
+    var form = new formidable.IncomingForm();
+
+    for (var key in options) {
+      form[key] = options[key];
+    }
+
+    form.parse(req, function(err, fields, files){
+      if (err) next(err);
+      req.body = fields;
+      req.files = files;
+      next();
+    });
+  }
+}
