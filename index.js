524e68169f1b6bb56702f4b07b7bd41e9ac4cc6c
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-01-13 03:40:16 +0100

    Updated to fix small bug

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports = function(options = {}){
   return function(req, res, next){
     if (req.method === 'GET' || req.method === 'DELETE')
       return next();
-    if (!req.headers['content-type'].includes('multipart/form-data'))
+    if (!req.headers
+        || !req.headers['content-type']
+        || !req.headers['content-type'].includes('multipart/form-data'))
       return next();
 
     var form = new formidable.IncomingForm();
